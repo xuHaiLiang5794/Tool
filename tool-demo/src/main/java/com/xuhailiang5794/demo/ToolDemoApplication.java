4b9461079563fc873a6e2568495c89717c1fb464
xuHaiLiang5794 <2764243724@qq.com>
2018-02-02 18:03:51 +0800

    添加一个用来做功能可视化的模块

---
+++ tool-demo/src/main/java/com/xuhailiang5794/demo/ToolDemoApplication.java
@@ -0,0 +1,21 @@
+package com.xuhailiang5794.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author hailiang.xu
+ * @version 1.0
+ * @since 2018/2/2 17:18
+ */
+@SpringBootApplication
+public class ToolDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolDemoApplication.class, args);
+    }
+}
